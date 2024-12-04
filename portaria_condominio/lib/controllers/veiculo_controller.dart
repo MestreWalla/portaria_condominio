2f074f5a01061f051ecf57b0a8707c1246d66aa0
ChristopherM51 <christophermatheus1@hotmail.com>
2024-12-03 21:35:14 -0300

    Merge branch 'main' of https://github.com/MestreWalla/portaria_condominio

---
+++ portaria_condominio/lib/controllers/veiculo_controller.dart
@@ -0,0 +1,31 @@
+import '../models/veiculo_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class VeiculoController {
+  final CollectionReference _veiculosCollection = FirebaseFirestore.instance.collection('veiculos');
+
+  Future<List<Veiculo>> listarVeiculos() async {
+    try {
+      final querySnapshot = await _veiculosCollection.get();
+      return querySnapshot.docs.map((doc) => Veiculo.fromMap(doc.data() as Map<String, dynamic>)).toList();
+    } catch (e) {
+      print('Error fetching vehicles: $e');
+      return [];
+    }
+  }
+
+  Future<void> cadastrarVeiculo(Veiculo veiculo) async {
+    // Simulate saving data to a database or API
+    await Future.delayed(Duration(seconds: 1));
+  }
+
+  Future<void> atualizarVeiculo(String placa, Map<String, String> updatedData) async {
+    // Simulate updating data in a database or API
+    await Future.delayed(Duration(seconds: 1));
+  }
+
+  Future<void> deletarVeiculo(String placa) async {
+    // Simulate deleting data from a database or API
+    await Future.delayed(Duration(seconds: 1));
+  }
+}
